f6f469ea60f768766cfda201e77c1e1815d7b10f
Yug-Shevarkar <yugshevarka@gmail.com>
2026-03-25 11:50:01 +0530

    Setup

---
+++ ItemProcessorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ItemProcessorApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
