5d9bf667b655d3ad36ea684d00ce14acfe075c1d
Yug-Shevarkar <yugshevarka@gmail.com>
2026-03-25 11:54:17 +0530

    Db Context Created

--- ItemProcessorApp/obj/Debug/net10.0/ItemProcessorApp.AssemblyInfo.cs
+++ ItemProcessorApp/obj/Debug/net10.0/ItemProcessorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemProcessorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f469ea60f768766cfda201e77c1e1815d7b10f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d10dcff011a647579272f95efac1eafdb1b33ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemProcessorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItemProcessorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
