7b246d53e3ca40c43c413bfd7ceb230a5683f3dc
Yug-Shevarkar <yugshevarka@gmail.com>
2026-03-25 13:34:58 +0530

    Temp Logic Updated

--- ItemProcessorApp/obj/Debug/net10.0/ItemProcessorApp.AssemblyInfo.cs
+++ ItemProcessorApp/obj/Debug/net10.0/ItemProcessorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemProcessorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcfa281cff11d947fc016796f7004c7faa6fc86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23b2d16e4594bf930d6d299a91b669568642456")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemProcessorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItemProcessorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
